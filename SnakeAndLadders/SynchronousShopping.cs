ab28f5940d6858f4f52df66e5ba96102cc209b6c
Ashley <ashleyjohn@gmail.com>
2016-06-02 00:16:50 +0530

    Added Djikstra's algo for Synchronous shopping problem

--- SnakeAndLadders/SynchronousShopping.cs
+++ SnakeAndLadders/SynchronousShopping.cs
@@ -16,6 +16,7 @@ namespace SnakeAndLadders
         public LinkedList<Tuple<int, int>>[] adjacencyList;
         public int[] fish;
         public int[,] distance;
+        public Queue<Tuple<int,Tuple<int,int>>> queue;
         public SynchronousShopping(int cities,int roads,int fishTypes)
         {
             this.cities = cities;
@@ -33,11 +34,12 @@ namespace SnakeAndLadders
         {
             int type = 0;
             for (int i = 1; i < input[0]; i++)
-                type = type | input[i];
+                type = type | (1 << (input[i] - 1));
             fish[index] = type;
         }
 
 
+        //Make sure to call bidirection
         public void AddEdgeAtEnd(int startVertex, int endVertex, int weight)
         {
             adjacencyList[startVertex].AddLast(new Tuple<int, int>(endVertex, weight));
@@ -53,9 +55,39 @@ namespace SnakeAndLadders
             for (int i = 1; i <= cities; i++)
                 for (int j = 0; j < fishTypes; j++)
                     distance[i, j] = Int32.MaxValue;
+            Push(1, fish[1], 0);
+            while(queue.Count > 0)
+            {
+                var x = queue.Dequeue();
+
+            //TODO - Get Vertex with min weight first;
+                foreach(var list in adjacencyList[x.Item2.Item1])
+                {
+                    Push(list.Item1, fish[list.Item1] | x.Item2.Item2, distance[x.Item2.Item1, x.Item2.Item2] + list.Item2);
+                }
+                    
+            }
+            int ret = Int32.MaxValue;
+            for(int i =0;i<(1 << fishTypes);i++)
+            {
+                for(int j=i;j<(1 << fishTypes);j++)
+                {
+                    if((i | j) == (1 << fishTypes) -1)
+                    {
+                        ret = Math.Min(ret, Math.Max(distance[cities, i], distance[cities, j]));
+                    }
+                }
+            }
 
         }
 
+        public void Push(int city,int fishTypes,int time)
+        {
+            if (distance[city, fishTypes] <= time)
+                return;
+            distance[city, fishTypes] = time;
+            queue.Enqueue(new Tuple<int, Tuple<int, int>>(time, new Tuple<int, int>(city, fishTypes)));
 
+        }
     }
 }
