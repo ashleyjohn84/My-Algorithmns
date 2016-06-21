9f72c81daccf59beb555e6717e5a5ef0e016ae0f
Ashley John Varghese <ashleyjohn@gmail.com>
2016-06-21 15:40:31 +0530

    CoinChange The DP way !!

--- SnakeAndLadders/CoinChangeProblem.cs
+++ SnakeAndLadders/CoinChangeProblem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace SnakeAndLadders
 {
@@ -18,5 +19,28 @@ namespace SnakeAndLadders
                 return 0;
             return GetCountOfSolutions(input, m - 1, Sum) + GetCountOfSolutions(input, m, Sum - input[m - 1]);
         }
+
+        public BigInteger GetCountOfSolutions1(int[] input, int m, int Sum)
+        {
+            BigInteger[,] table = new BigInteger[Sum+1,m];
+
+            for (int i = 0; i < m; i++)
+                table[0,i] = 1;
+            for(int i =1;i<=Sum;i++)
+            {
+                for(int j =0;j<m;j++)
+                {
+                    BigInteger x = i - input[j] >= 0 ? table[i - input[j], j] : 0;
+
+                    BigInteger y = j >= 1 ? table[i, j - 1] : 0;
+
+                    table[i, j] = x + y;
+                }
+            }
+            return table[Sum, m-1];
+
+        }
+
+
     }
 }
--- SnakeAndLaddersTests/CoinChangeProblemTests.cs
+++ SnakeAndLaddersTests/CoinChangeProblemTests.cs
@@ -18,5 +18,22 @@ namespace SnakeAndLadders.Tests
             int[] array = new int[] {1,2,3 };
             var c = obj.GetCountOfSolutions(array, 3, 4);
         }
+
+        [TestMethod()]
+        public void GetCountOfSolutionsTest1()
+        {
+            CoinChangeProblem obj = new CoinChangeProblem();
+            int[] array = new int[] { 5, 37, 8, 39, 33, 17, 22, 32, 13, 7, 10, 35, 40,
+                2, 43, 49, 46, 19, 41, 1, 12, 11, 28 };
+            var c = obj.GetCountOfSolutions1(array, 23, 166);
+        }
+
+        [TestMethod()]
+        public void GetCountOfSolutionsTest2()
+        {
+            CoinChangeProblem obj = new CoinChangeProblem();
+            int[] array = new int[] { 1,2};
+            var c = obj.GetCountOfSolutions(array, 2, 3);
+        }
     }
 }
