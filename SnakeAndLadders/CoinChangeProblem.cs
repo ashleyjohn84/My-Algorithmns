527ce0bc6c10fe9c827492e99bb7b82aafa81abc
Ashley John Varghese <ashleyjohn@gmail.com>
2016-06-21 14:36:26 +0530

    Coin Change Problem : Total no of Solutions

---
+++ SnakeAndLadders/CoinChangeProblem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadders
+{
+    public class CoinChangeProblem
+    {
+        public int GetCountOfSolutions(int[] input,int m,int Sum)
+        {
+            if (Sum == 0)
+                return 1;
+            if (Sum < 0)
+                return 0;
+            if (m <= 0 && Sum > 0)
+                return 0;
+            return GetCountOfSolutions(input, m - 1, Sum) + GetCountOfSolutions(input, m, Sum - input[m - 1]);
+        }
+    }
+}
---
+++ SnakeAndLaddersTests/CoinChangeProblemTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SnakeAndLadders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadders.Tests
+{
+    [TestClass()]
+    public class CoinChangeProblemTests
+    {
+        [TestMethod()]
+        public void GetCountOfSolutionsTest()
+        {
+            CoinChangeProblem obj = new CoinChangeProblem();
+            int[] array = new int[] {1,2,3 };
+            var c = obj.GetCountOfSolutions(array, 3, 4);
+        }
+    }
+}
