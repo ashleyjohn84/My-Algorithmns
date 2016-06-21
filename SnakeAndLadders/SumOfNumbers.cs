9e696784d3e2b35a96f719bc84718e2ec75b42db
Ashley John Varghese <ashleyjohn@gmail.com>
2016-06-21 16:33:13 +0530

    SubSet Sum Problem DP

--- SnakeAndLadders/SumOfNumbers.cs
+++ SnakeAndLadders/SumOfNumbers.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit;
 using NUnit.Framework;
-// Check if sum of a few elements match up to a number
+// Check if sum of a few elements(subset) match up to a number
 namespace SnakeAndLadders
 {
     public class SumOfNumbers
@@ -21,6 +21,33 @@ namespace SnakeAndLadders
             var a = isPossible(array, currentSum + array[start], finalSum, start + 1, end);
             var b = isPossible(array, currentSum, finalSum, start + 1, end);
             return a || b;
+        }
+
+
+        public static bool  IsPossibleDP(int[] array,int Sum,int n)
+        {
+            bool[,] result = new bool[Sum + 1, n+1];
+            for(int i = 0; i <= n; i++)
+            {
+                result[0, i] = true;
+            }
+            for (int i = 1; i <= Sum; i++)
+            {
+                result[i, 0] = false;
+            }
+
+            for(int i = 1; i <= Sum; i++)
+            {
+                for(int j =1; j <= n; j++)
+                {
+                    result[i, j] = result[i, j - 1];
+                    if (i >= array[j - 1])
+                        result[i, j] = result[i, j] || result[i - array[j - 1], j - 1];
+                }
+            }
+
+            return result[Sum, n];
+
         }
 
        
